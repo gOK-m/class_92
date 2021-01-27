7394285b89f868d8cbfa5cd4b3277ae96d5c96e2
gOK-m <71651713+gOK-m@users.noreply.github.com>
2021-01-27 17:35:28 +0530

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,7 @@
+function back(){
+    window.location = "activity_1.html";
+}
+function getScore(){
+    score = localStorage.getItem("score");
+    document.getElementById("update").innerHTML = "score:" + score;
+}
